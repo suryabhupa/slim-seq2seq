20694cef0aed969f9841241cfdbd29454dd00211
Surya Bhupatiraju <surya95@gmail.com>
2016-12-24 09:15:39 +0000

    more fixes [incomplete]

--- old_seq2seq.py
+++ old_seq2seq.py
@@ -30,7 +30,6 @@ a very similar interface, so after reading the above tutorial and using
 one of them, others should be easy to substitute.
 
 * Full sequence-to-sequence models.
-  - basic_rnn_seq2seq: The most basic RNN-RNN model.
   - embedding_attention_seq2seq: Advanced model with input embedding and
       the neural attention mechanism; recommended for complex tasks.
 
@@ -104,76 +103,6 @@ def _extract_argmax_and_embed(embedding, output_projection=None,
     return emb_prev
   return loop_function
 
-
-def rnn_decoder(decoder_inputs, initial_state, cell, loop_function=None,
-                scope=None):
-  """RNN decoder for the sequence-to-sequence model.
-
-  Args:
-    decoder_inputs: A list of 2D Tensors [batch_size x input_size].
-    initial_state: 2D Tensor with shape [batch_size x cell.state_size].
-    cell: rnn_cell.RNNCell defining the cell function and size.
-    loop_function: If not None, this function will be applied to the i-th output
-      in order to generate the i+1-st input, and decoder_inputs will be ignored,
-      except for the first element ("GO" symbol). This can be used for decoding,
-      but also for training to emulate http://arxiv.org/abs/1506.03099.
-      Signature -- loop_function(prev, i) = next
-        * prev is a 2D Tensor of shape [batch_size x output_size],
-        * i is an integer, the step number (when advanced control is needed),
-        * next is a 2D Tensor of shape [batch_size x input_size].
-    scope: VariableScope for the created subgraph; defaults to "rnn_decoder".
-
-  Returns:
-    A tuple of the form (outputs, state), where:
-      outputs: A list of the same length as decoder_inputs of 2D Tensors with
-        shape [batch_size x output_size] containing generated outputs.
-      state: The state of each cell at the final time-step.
-        It is a 2D Tensor of shape [batch_size x cell.state_size].
-        (Note that in some cases, like basic RNN cell or GRU cell, outputs and
-         states can be the same. They are different for LSTM cells though.)
-  """
-  with variable_scope.variable_scope(scope or "rnn_decoder"):
-    state = initial_state
-    outputs = []
-    prev = None
-    for i, inp in enumerate(decoder_inputs):
-      if loop_function is not None and prev is not None:
-        with variable_scope.variable_scope("loop_function", reuse=True):
-          inp = loop_function(prev, i)
-      if i > 0:
-        variable_scope.get_variable_scope().reuse_variables()
-      output, state = cell(inp, state)
-      outputs.append(output)
-      if loop_function is not None:
-        prev = output
-  return outputs, state
-
-
-def basic_rnn_seq2seq(
-    encoder_inputs, decoder_inputs, cell, dtype=dtypes.float32, scope=None):
-  """Basic RNN sequence-to-sequence model.
-
-  This model first runs an RNN to encode encoder_inputs into a state vector,
-  then runs decoder, initialized with the last encoder state, on decoder_inputs.
-  Encoder and decoder use the same RNN cell type, but don't share parameters.
-
-  Args:
-    encoder_inputs: A list of 2D Tensors [batch_size x input_size].
-    decoder_inputs: A list of 2D Tensors [batch_size x input_size].
-    cell: rnn_cell.RNNCell defining the cell function and size.
-    dtype: The dtype of the initial state of the RNN cell (default: tf.float32).
-    scope: VariableScope for the created subgraph; default: "basic_rnn_seq2seq".
-
-  Returns:
-    A tuple of the form (outputs, state), where:
-      outputs: A list of the same length as decoder_inputs of 2D Tensors with
-        shape [batch_size x output_size] containing the generated outputs.
-      state: The state of each decoder cell in the final time-step.
-        It is a 2D Tensor of shape [batch_size x cell.state_size].
-  """
-  with variable_scope.variable_scope(scope or "basic_rnn_seq2seq"):
-    _, enc_state = rnn.rnn(cell, encoder_inputs, dtype=dtype)
-    return rnn_decoder(decoder_inputs, enc_state, cell)
 
 def attention_decoder(decoder_inputs,
                       initial_state,
@@ -482,6 +411,7 @@ def embedding_attention_seq2seq(encoder_inputs,
     # First calculate a concatenation of encoder outputs to put attention on.
     top_states = [array_ops.reshape(e, [-1, 1, cell.output_size])
                   for e in encoder_outputs]
+    # attention_states = array_ops.concat(1, top_states)
     attention_states = array_ops.concat(1, top_states)
 
     print("HELLOOOO")
